0b1004fd88f5c725c0f37b8d1491db8d75796982
Ukuma012 <ukuma012@gmail.com>
2023-05-11 21:36:14 +0900

    sockaddr_in

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+
+// struct in_addr
+// {
+//         ip_addr_t s_addr;
+// };
+
+//  struct sockaddr_in {
+//      unsigned char  sin_len;
+//      unsigned char  sin_family;
+//      unsigned short sin_port;
+//      struct in_addr sin_addr;
+//      unsigned char  sin_zero[8];
+
+// };
+
+struct sockaddr_in socketaddress;
 
 int main(int argc, char *argv[]) {
     int socketfd;
@@ -8,6 +26,9 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "socket failed\n");
         exit(1);
     }
+
+    memset(&socketaddress, 0, sizeof(socketaddress));
+    socketaddress.sin_family = AF_INET;
 
     printf("%d\n", socketfd);
     exit(0);
